dcb9b9e96a7fc0fe24cbf940d9bebb27a08c06cb
李锦鸿 <mac@OOoo00O0oo00.local>
2026-04-10 01:05:48 +0800

    Add Footer blog button.

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -18,10 +18,16 @@ export default async function Footer() {
     <footer className="text-sm text-neutral-500 dark:text-neutral-400">
       <ContactUs/>
       <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm dark:border-neutral-700 md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0">
-        <div>
+        <div className="flex w-full items-center justify-between">
           <Link className="flex items-center gap-2 text-black dark:text-white md:pt-1" href="/">
             <LogoSquare size="sm" />
             <span className="uppercase">{SITE_NAME}</span>
+          </Link>
+           <Link 
+            href="/blog" 
+            className="pr-[50px] text-neutral-500/70 hover:text-black dark:hover:text-white transition-colors font-medium uppercase"
+          >
+            Blog
           </Link>
         </div>
         <Suspense
