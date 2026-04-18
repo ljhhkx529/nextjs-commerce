92e51e2a4f48067479aadd0fa0358223ff41830e
Oleg <lihzyx520@gmail.com>
2026-04-18 14:33:13 +0000

    Add ahrefs metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from 'components/layout/navbar';
 import { ensureStartsWith } from 'lib/utils';
 import { Inter } from 'next/font/google';
+import Script from 'next/script'; // 引入 Next.js 优化的脚本组件
 import { ReactNode, Suspense } from 'react';
 import './globals.css';
 
@@ -18,6 +19,14 @@ export const metadata = {
   robots: {
     follow: true,
     index: true
+  },
+    // 1. 在这里添加验证信息
+  verification: {
+    yandex: 'be92de15e9967681',
+      other: {
+    'ahrefs-site-verification': 'fbf6fe6d922f3ffe776b06cec04ba65861270c657f5ef43d706e5a10652b5722',
+  },
+    // google: '这里可以放你的 Google GSC 验证码', 
   },
 
   // ✅ 加这一段（核心）
@@ -45,6 +54,12 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
   return (
     <html lang="en" className={inter.variable}>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
+                {/* ✅ 添加 Ahrefs 统计脚本 */}
+        <Script 
+          src="https://analytics.ahrefs.com/analytics.js" 
+          data-key="aewmjkaynpCAVDKd5Mtq3A" 
+          strategy="afterInteractive" // 性能优化：在页面交互后再加载
+        />
         <Navbar />
         <Suspense>
           <main>{children}</main>
